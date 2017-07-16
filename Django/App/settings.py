2ce9b09caef4b805921c63e441607908971371f2
Joel Evans <joelevans@Joels-MacBook-Pro.local>
2017-07-15 20:39:40 -0700

    work

--- Django/App/settings.py
+++ Django/App/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+
 ]
 
 ROOT_URLCONF = 'App.urls'
